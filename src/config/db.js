82e997e2cfd2e2bc28979c51b7dd95fd13868395
ilyaasomar <ilyaascumar10@gmail.com>
2026-03-01 14:38:46 +0100

    #refactor some errors

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.ts";
+// import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 const connectionString = process.env.DATABASE_URL;
 const adapter = new PrismaPg({ connectionString });
