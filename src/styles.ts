1c696c8227609a29b80b262404ed5e213fc23c12
ilyaasomar <ilyaascumar10@gmail.com>
2026-03-01 14:54:19 +0100

    dashboard in progress

--- src/styles.ts
+++ src/styles.ts
@@ -1,14 +1,3 @@
-// export const styles = {
-//   primaryBgColor: "bg-[#4191F9]", // Bright Blue
-//   secondaryBgColor: "bg-[#10B981]", // Emerald Green
-//   accentBgColor: "bg-[#6B7280]", // Gray
-//   primaryColor: "text-[#4191F9]", // Bright Blue
-//   secondaryColor: "text-[#10B981]", // Emerald Green
-//   accentColor: "text-[#6B7280]", // Gray
-// };
-
-// // Hover/Active links,Active/Highlight bars/lines: Primary Blue underline or text color
-// // charts, Success messages,Positive values,Success Button (Confirm, Pay, Save): Emeral Green
 
 export const styles = {
   primaryBgColor: "bg-[#1C1C1C]",
