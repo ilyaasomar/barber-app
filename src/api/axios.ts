7ff2f7027378bc4da55ddbf043c2e39de019da35
ilyaasomar <ilyaascumar10@gmail.com>
2026-03-01 15:45:57 +0100

    fix: add vercel rewrite for SPA routing

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://localhost:8000/api", //after deploy i have to change to the backend url
 
-  baseURL: "https://barberappsw-backend.up.railway.app/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 
